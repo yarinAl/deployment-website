b6343573d9c0f634f54250db582f1b21764241b7
yarin <yarin@gmail.com>
2022-09-24 15:06:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+const goto = () => {
+  window.location.href = 'https://www.github.com';
+}
+const goto2 = () => {
+  window.location.href = 'https://yarin-website.netlify.app/';
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="MainContainer">
+      <div className='MainHeader'>
+        
+        <div className='topMain'>
+
+        <button onClick={() => { goto2() }} className='btnTop'>
+
+<div className='innerBtnText'>
+  Shopping list App
+</div>
+</button>
+        <button onClick={() => { goto() }} className='btnTop'>
+
+            <div className='innerBtnText'>
+              My Github
+            </div>
+          </button>
+          Yarin's Website
+        </div>
+        <div className='navBarMain'>
+          <ul>
+            <li>Home</li>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <li>News</li>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <li>Contact</li>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <li>About</li>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+          </ul>
+        </div>
+      </div>
+      <div className='BodyImage'></div>
+      <div className='bottomMain'></div>
     </div>
   );
 }
